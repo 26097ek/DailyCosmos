42df20c34edc879c871fefb1235db11ab9af8399
26097ek <26097ek@hvhs.school.nz>
2026-07-26 17:46:40 +1200

    Add Data Fetcher

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import './style.css'
 
+const API_KEY = import.meta.env.VITE_NASA_API_KEY
 
-document.querySelector('#app').innerHTML = `
-`
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+.then(response => response.json())
+.then(data => {console.log(data); return data;})
+.then(data => {
+    document.querySelector('#app').innerHTML = `
+    <h1>${data.title}</h1>
+    ${data.media_type === "video" ? `<video controls src="${data.url}"></video>` : `<img src="${data.url}">`}
+    ${data.copywright ? `<p>${data.copywright}</p>` : ""}
+    <br>
+    <p>${data.explanation}</p>
+    `
+})
