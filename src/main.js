97444963eb28d98f6e63ff5067f0a6d29be3db68
26097ek <26097ek@hvhs.school.nz>
2026-07-28 19:20:54 +1200

    Add CSS

--- src/main.js
+++ src/main.js
@@ -2,14 +2,25 @@ import './style.css'
 
 const API_KEY = import.meta.env.VITE_NASA_API_KEY
 
+document.querySelector("#app").innerHTML = `<span style="font-size: 10rem;"><h1 id="loading">Stealing data from NASA...</h1></span>`
+
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
 .then(response => response.json())
 .then(data => {console.log(data); return data;})
 .then(data => {
+    let img;
+    if (data.media_type === "image") {
+        img = `<img src="${data.url}" id="img">`
+    } else if (data.url.includes("youtube")) {
+        img = `<iframe src="${data.url}" id="img" />`
+    } else {
+        img = `<video controls src="${data.url}" id="img"></video>`
+    }
     document.querySelector('#app').innerHTML = `
+    <h2>The Daily Cosmos</h2>
     <h1>${data.title}</h1>
-    ${data.media_type === "video" ? `<video controls src="${data.url}"></video>` : `<img src="${data.url}">`}
-    ${data.copywright ? `<p>${data.copywright}</p>` : ""}
+    ${img}
+    ${data.copyright ? `<p>${"Copyright " + data.copyright}</p>` : ""}
     <br>
     <p>${data.explanation}</p>
     `
